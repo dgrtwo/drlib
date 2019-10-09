1780f937f0d488b56c0e23db92e2e976cbd9c371
David Robinson <david.robinson@flatiron.com>
2019-10-09 10:20:48 -0400

    Expand on expand_strata examples

--- R/extract-strata.R
+++ R/extract-strata.R
@@ -1,9 +1,11 @@
 #' Extract strata from a tidied survival object
 #'
+#' The tidied results of a survival object are in
+#'
 #' @param tbl A table
 #' @param column Column to extract. Should be of the form \code{col1=val1, col2=val2, ...}
 #' @param ... Extra arguments passed on to \link[tidyr]{extract}, such as \code{remove} or
-#' \code{convert}.df
+#' \code{convert}.
 #'
 #' @importFrom rlang !!
 #'
@@ -12,12 +14,24 @@
 #' library(survival)
 #' library(broom)
 #'
-#' surv_fit <- survfit(Surv(stop, status) ~ sex + pcdx,
-#'                     data = mgus1, subset = (start == 0))
+#' surv_td <- survfit(Surv(stop - start, status) ~ sex + pcdx,
+#'                     data = mgus1) %>%
+#'   tidy()
 #'
+#' surv_td
+#'
+#' # Extract into "sex" and "pcdx" columns
 #' surv_fit %>%
-#'   tidy() %>%
 #'   extract_strata(strata)
+#'
+#' # Now visualize, separating by strata
+#' library(ggplot2)
+#' surv_fit %>%
+#'   extract_strata(strata) %>%
+#'   ggplot(aes(time, estimate, color = sex)) +
+#'   geom_line() +
+#'   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .25) +
+#'   facet_wrap(~ pcdx)
 #'
 #' @export
 extract_strata <- function(tbl, column, ...) {
